e47334efd6b4a7b7ec6ae4bcd0dc5c513b243568
JoJoJoJoestar <100035640+JoJoJoJoestar@users.noreply.github.com>
2023-09-30 17:45:34 +0200

    Ui

---
+++ GameJam/Assets/InGameUI.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameUI : MonoBehaviour
+{
+    public List<Sprite> BarImages;
+    public List<Sprite> RoleImages;
+}
